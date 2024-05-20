8fc3d3645ed5571f78614dffcc0eeb6483159acc
Debadrita Mondal <111679707+mondaldebadrita03@users.noreply.github.com>
2024-05-20 21:01:34 +0530

    Update bubble_Sort.c

--- bubble_Sort.c
+++ bubble_Sort.c
@@ -1,6 +1,6 @@
 #include<stdio.h>
 void bubble_Sort(int arr[],int n){
-    for(int i=0;i<=n-1;i++) {
+    for(int i=0;i<n-1;i++) {
         int count=0;
         for(int j=0;j<n-i-1;j++){
             if(arr[j]>arr[j+1]){
