2324566359d66080477ebf3d0bf38b463fc021d8
Debadrita Mondal <111679707+mondaldebadrita03@users.noreply.github.com>
2024-05-22 17:42:28 +0530

    Update m_Colouring.c

--- m_Colouring.c
+++ m_Colouring.c
@@ -1,54 +1,57 @@
-#include <stdbool.h>
-#include <stdio.h>
-#define V 4
-void printSolution(int color[]);
-bool isSafe(int v, bool graph[V][V], int color[], int c){
-    for (int i = 0; i < V; i++)
-        if (graph[v][i] && c == color[i])
-            return false;
-    return true;
+#include<stdio.h>
+#include<stdlib.h>
+#define MAX_NODES 100
+int G[MAX_NODES][MAX_NODES];
+int *x;
+int n,m;
+void nextValue(int k){
+    int j;
+    do{
+        x[k]=(x[k]+1)%(m+1) ; // Next Highest color
+        if(x[k]==0)      // Alll colors have been used
+            return ;
+        for(j=1;j<=n;j++) {
+          if((G[k][j]!=0) &&(x[k]==x[j])) // Check if this color is distinct from adjacent colors
+              // If (k,j) is an edge and if adjacent vertices have same color
+              break;  
+          }
+	  if(j==n+1) // New color found
+             return; 
+    } while(1);//Otherwise try to find another color
 }
-bool graphColoringUtil(bool graph[V][V], int m, int color[],
-                       int v){
-    if (v == V)
-        return true;
-    for (int c = 1; c <= m; c++) {
-        if (isSafe(v, graph, color, c)) {
-            color[v] = c;
-            if (graphColoringUtil(graph, m, color, v + 1)
-                == true)
-                return true;
-            color[v] = 0;
-        }
-    }
-    return false;
+void mcoloring(int k){
+	int i;
+    do{
+       nextValue(k);   // Assign to x[k] a legal color
+       if(x[k]==0)     // no new color is possible
+	   		return;
+       if(k==n)        // At most m colors have been used to color n vertices
+       {
+       	for(i=1;i<=n;i++){
+       		printf("%d\t",x[i]);
+		}
+		printf("\n");
+	   }
+       else
+          mcoloring(k+1);	
+    }while(1);    
 }
-bool graphColoring(bool graph[V][V], int m){
-    int color[V];
-    for (int i = 0; i < V; i++)
-        color[i] = 0;
-    if (graphColoringUtil(graph, m, color, 0) == false) {
-        printf("Solution does not exist");
-        return false;
-    }
-    printSolution(color);
-    return true;
-}
-void printSolution(int color[]){
-    printf("Solution Exists:"
-           " Following are the assigned colors \n");
-    for (int i = 0; i < V; i++)
-        printf(" %d ", color[i]);
-    printf("\n");
-}
-int main(){
-    bool graph[V][V] = {
-        { 0, 1, 1, 1 },
-        { 1, 0, 1, 0 },
-        { 1, 1, 0, 1 },
-        { 1, 0, 1, 0 },
-    };
-    int m = 3;
-    graphColoring(graph, m);
-    return 0;
+int main(void){
+	int i,j;
+	printf("Enter the number of nodes:");
+	scanf("%d",&n);
+	printf("Enter the number of colors:");
+	scanf("%d",&m);
+	printf("Enter the adjacency matrix:\n");
+	for(i=1;i<=n;i++){
+		for(j=1;j<=n;j++){
+			scanf("%d",&G[i][j]);
+		}
+	}
+	x=(int *)malloc(n*sizeof(int));
+	for(i=1;i<=n;i++){
+		x[i]=0; //*(x+i)=0
+	}
+	mcoloring(1);	
+	return 0;		
 }
